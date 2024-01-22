6085bdbb046654c9a876cb6be51a53460f8c9a77
Henrique Delavi Daum <henrique.delavi.daum@gmail.com>
2024-01-21 22:38:09 -0300

    feat: add pyautogui to insert file path send .pdf as attachment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import time
+import pyautogui
 import pandas as pd
 
 from dotenv import load_dotenv
@@ -25,48 +26,55 @@ for i, name in enumerate(contatos_df['nome']):
     contact_phone = contatos_df.loc[i, 'numero']
     contact_name = contatos_df.loc[i, 'nome']
     contact_file = contatos_df.loc[i, 'ficha_glp']
+    print(f"contact_file --> {contact_file}")
 
     # search for contact since all of them is listed
-    search_input = browser.find_element(By.XPATH, '//*[@id="side"]/div[1]/div/div[2]/div[2]/div/div[1]/p')
-    search_input.send_keys(str(contact_phone))
+    browser.find_element(By.XPATH, '//*[@id="side"]/div[1]/div/div[2]/div[2]/div/div[1]/p').send_keys(str(contact_phone))
     time.sleep(5)
 
     # click on the first match given should have only one
-    browser.find_element(By.XPATH, '//*[@id="pane-side"]/div[1]/div/div/div[1]/div/div/div/div[1]').click()
+    browser.find_element(By.XPATH, '//*[@id="pane-side"]/div[1]/div/div/div/div/div').click()
     time.sleep(5)
     
     # write the messages
     print('writing messages')
-    text_field = browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
-    text_field.send_keys(message_hello.format(contact_name))
-    text_field.send_keys(Keys.ENTER)
+    browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p').send_keys(message_hello.format(contact_name))
+    browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p').send_keys(Keys.ENTER)
     time.sleep(10)
     
-    text_field = browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
+    browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
     for char in message1:
-        text_field.send_keys(char)
+        browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p').send_keys(char)
         # time.sleep(0.1)
-    text_field.send_keys(Keys.ENTER)
-    time.sleep(10)
+    browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p').send_keys(Keys.ENTER)
+    time.sleep(7)
     
-    text_field = browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
+    browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
     for char in message2:
-        text_field.send_keys(char)
+        browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p').send_keys(char)
         # time.sleep(0.1)
-    text_field.send_keys(Keys.ENTER)
-    time.sleep(10)
+    browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p').send_keys(Keys.ENTER)
+    time.sleep(7)
 
     print(os.path.abspath(f"data\\{contact_file}"))
     # find button to add attachment and send it
     browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div').click()
+    time.sleep(2)
+
+    browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/ul/div/div[1]/li/div/span').click()
     time.sleep(3)
-    browser.find_element(By.CSS_SELECTOR, 'input[type="file"]').send_keys(os.path.abspath(f"data\\{contact_file}"))
-    browser.find_element(By.CSS_SELECTOR, 'input[type="file"]').send_keys(Keys.ENTER)
-    browser.find_element(By.CSS_SELECTOR, 'data-icon="send"').click()
+
+    pyautogui.write(os.path.abspath(f"data\\{contact_file}"))
+    pyautogui.press('enter')
+    time.sleep(5)
+
+    browser.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]/div/div').click()
 
     # waiting to end it all
-    counter = 20
+    counter = 10
     while counter > 0:
         print(counter)
         time.sleep(1)
         counter -= 1
+
+print("SUCCESS!")
