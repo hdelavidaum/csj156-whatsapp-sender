623952cd781e525f1643155875990728b97d5eae
Henrique Delavi Daum <henrique.delavi.daum@gmail.com>
2024-01-21 21:12:26 -0300

    feat: open browser, login to whatsapp, find the receiver and send text message

---
+++ main.py
@@ -0,0 +1,72 @@
+import os
+import time
+import pandas as pd
+
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+load_dotenv()
+
+message_hello = os.getenv('MESSAGE_HELLO')
+message1 = os.getenv('MESSAGE_1')
+message2 = os.getenv('MESSAGE_2')
+
+browser = webdriver.Chrome()
+browser.get("https://web.whatsapp.com/")
+while len(browser.find_elements(By.ID, "side")) < 1:
+    time.sleep(10)
+
+contatos_df = pd.read_excel("data/contacts.xlsx")
+for i, name in enumerate(contatos_df['nome']):
+    print(f"Starting if with element #{i}")
+    contact_id = i
+    contact_phone = contatos_df.loc[i, 'numero']
+    contact_name = contatos_df.loc[i, 'nome']
+    contact_file = contatos_df.loc[i, 'ficha_glp']
+
+    # search for contact since all of them is listed
+    search_input = browser.find_element(By.XPATH, '//*[@id="side"]/div[1]/div/div[2]/div[2]/div/div[1]/p')
+    search_input.send_keys(str(contact_phone))
+    time.sleep(5)
+
+    # click on the first match given should have only one
+    browser.find_element(By.XPATH, '//*[@id="pane-side"]/div[1]/div/div/div[1]/div/div/div/div[1]').click()
+    time.sleep(5)
+    
+    # write the messages
+    print('writing messages')
+    text_field = browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
+    text_field.send_keys(message_hello.format(contact_name))
+    text_field.send_keys(Keys.ENTER)
+    time.sleep(10)
+    
+    text_field = browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
+    for char in message1:
+        text_field.send_keys(char)
+        # time.sleep(0.1)
+    text_field.send_keys(Keys.ENTER)
+    time.sleep(10)
+    
+    text_field = browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
+    for char in message2:
+        text_field.send_keys(char)
+        # time.sleep(0.1)
+    text_field.send_keys(Keys.ENTER)
+    time.sleep(10)
+
+    print(os.path.abspath(f"data\\{contact_file}"))
+    # find button to add attachment and send it
+    browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div').click()
+    time.sleep(3)
+    browser.find_element(By.CSS_SELECTOR, 'input[type="file"]').send_keys(os.path.abspath(f"data\\{contact_file}"))
+    browser.find_element(By.CSS_SELECTOR, 'input[type="file"]').send_keys(Keys.ENTER)
+    browser.find_element(By.CSS_SELECTOR, 'data-icon="send"').click()
+
+    # waiting to end it all
+    counter = 20
+    while counter > 0:
+        print(counter)
+        time.sleep(1)
+        counter -= 1
