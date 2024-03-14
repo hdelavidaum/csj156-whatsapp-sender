04ea348d8d9c942063dcc0eeaa8bf9d31c2e94d7
Henrique Delavi Daum <henrique.delavi.daum@gmail.com>
2024-03-13 22:12:30 -0300

    feat: tweak to send image and deal with ddi too

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ message2 = os.getenv('MESSAGE_2')
 message3 = os.getenv('MESSAGE_3')
 message4 = os.getenv('MESSAGE_4')
 message5 = os.getenv('MESSAGE_5')
+whatsapp_contact_url = "https://web.whatsapp.com/send?phone={}"
 
 browser = webdriver.Chrome()
 browser.get("https://web.whatsapp.com/")
@@ -24,23 +25,23 @@ while len(browser.find_elements(By.ID, "side")) < 1:
 
 xpath_write_message = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p'
 
-contatos_df = pd.read_excel("data/contacts.xlsx")
+# contatos_df = pd.read_excel("data/contacts.xlsx")
+contatos_df = pd.read_excel("data/contacts-test.xlsx")
 for i, name in enumerate(contatos_df['nome']):
     print(f"Starting if with element #{i}")
     contact_id = i
     contact_user_num = contatos_df.loc[i, 'cadastro']
     contact_phone = contatos_df.loc[i, 'numero']
     contact_name = contatos_df.loc[i, 'nome']
-    contact_file = contatos_df.loc[i, 'ficha_glp']
-    print(f"contact_file --> {contact_file}")
+    contact_file = contatos_df.loc[i, 'extrato']
 
-    # search for contact since all of them is listed
-    browser.find_element(By.XPATH, '//*[@id="side"]/div[1]/div/div[2]/div[2]/div/div[1]/p').send_keys(str(contact_phone))
-    time.sleep(5)
-
-    # click on the first match given should have only one
-    browser.find_element(By.XPATH, '//*[@id="pane-side"]/div[1]/div/div/div/div/div').click()
-    time.sleep(5)
+    print(f"Going to {whatsapp_contact_url.format(contact_phone)}")
+    browser.get(whatsapp_contact_url.format(contact_phone))
+    counter = 45
+    while counter > 0:
+        print(counter)
+        time.sleep(1)
+        counter -= 1
     
     # write the messages
     print('writing messages')
@@ -56,29 +57,34 @@ for i, name in enumerate(contatos_df['nome']):
     browser.find_element(By.XPATH, xpath_write_message).send_keys(Keys.ENTER)
     time.sleep(2)
 
-    # find plus sign button to add attachment
-    browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div').click()
-    time.sleep(2)
-
-    # find Documento button to add attachment
-    browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/ul/div/div[1]/li/div/span').click()
-    time.sleep(3)
-
-    # Insert file path and attach it
-    pyautogui.write(os.path.abspath(f"data\\{contact_file}"))
-    pyautogui.press('enter')
-    time.sleep(5)
-
-    # Click to send the file
-    browser.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]/div/div').click()
-
-    browser.find_element(By.XPATH, xpath_write_message).send_keys(message3.format(contact_user_num))
+    browser.find_element(By.XPATH, xpath_write_message).send_keys(message3)
     browser.find_element(By.XPATH, xpath_write_message).send_keys(Keys.ENTER)
     time.sleep(2)
 
     browser.find_element(By.XPATH, xpath_write_message).send_keys(message4)
     browser.find_element(By.XPATH, xpath_write_message).send_keys(Keys.ENTER)
     time.sleep(2)
+
+    # find plus sign button to add attachment
+    browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div').click()
+    time.sleep(2)
+
+    # find Documento button to add attachment
+    # browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/ul/div/div[1]/li/div/span').click()
+    # time.sleep(3)
+
+    # find Fotos e Vídeos button to add attachment
+    browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/ul/div/div[2]/li/div/span').click()
+    time.sleep(3)
+
+    # Insert file path and attach it
+    pyautogui.write(os.path.abspath(f"data\\tesouraria\\{contact_file}.png"))
+    pyautogui.press('enter')
+    time.sleep(5)
+
+    # Click to send the file
+    browser.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]/div/div').click()
+    time.sleep(4)
 
     browser.find_element(By.XPATH, xpath_write_message).send_keys(message5)
     browser.find_element(By.XPATH, xpath_write_message).send_keys(Keys.ENTER)
@@ -87,7 +93,7 @@ for i, name in enumerate(contatos_df['nome']):
     # waiting to end it all
     counter = 10
     while counter > 0:
-        print(counter)
+        print(f"Ending in {counter}")
         time.sleep(1)
         counter -= 1
 
